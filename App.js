dc4ad0f843c2ba5be9b11d2fbc901561b939a514
Tomi Dwi Saputra <tomidwisaputra89@gmail.com>
2024-11-22 20:22:53 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>TUGAS P2 PRAKTIKUM DPM</Text>
+
+      <View style={styles.boxContainer}>
+        <View style={[styles.box, styles.box1]} />
+        <View style={[styles.box, styles.box2]} />
+      </View>
     </View>
   );
 }
@@ -13,8 +17,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#D3D3D3",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    color: "blue",
+    fontWeight: "bold",
+    marginBottom: 50,
+  },
+  boxContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+  box1: {
+    backgroundColor: "#511845",
+  },
+  box2: {
+    backgroundColor: "#A05344",
   },
 });
